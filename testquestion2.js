4e8d63073f13de9f5ffb54baae4483097ca0759e
javen2015 <zqlin15@gmail.com>
2025-08-30 15:16:09 +0800

    Update testquestion2.js

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,12 @@
+function setup(){
+    createCanvas(600,600);
+    background(100);
+}
+function draw(){
+    for (let i = 0; i < 10; i++) {
+        fill(i*20);
+        //11 gaps
+        circle(((width/11)*i)+50,((height/11)*i)+30,50,50);
+    }
+}
 
